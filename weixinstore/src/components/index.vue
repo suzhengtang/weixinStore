338421cf483f02f7ae2c445a68aad94a69dde498
suzhengtang <1037617576@qq.com>
2018-08-15 23:56:54 +0800

    更新项目初始化

---
+++ weixinstore/src/components/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="box">
+    <p>{{ msg }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {
+      msg: '我是测试的文字'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+  .box{
+    p {
+      color: red;
+    }
+  }
+</style>
