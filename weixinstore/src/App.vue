0ef486da532dc662e80389f4d9acffa0f9328715
suzhengtang <1037617576@qq.com>
2018-08-27 23:33:17 +0800

    单位换算OK

--- weixinstore/src/App.vue
+++ weixinstore/src/App.vue
@@ -4,9 +4,7 @@
   </div>
 </template>
 
-<script src="assets/js/base.js"></script>
 <script>
-// import { base } from "assets/js/base.js";
 export default {
   name: 'App'
 }
@@ -14,9 +12,4 @@ export default {
 
 <style lang="less">
 @import "assets/less/base.less";
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-}
 </style>
--- weixinstore/src/components/index.vue
+++ weixinstore/src/components/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="box">
-    <p>{{ msg }}</p>
+    <p class="ceshi1">{{ msg }}</p>
+    <p class="ceshi2">{{ msg }}</p>
   </div>
 </template>
 
@@ -9,7 +10,7 @@ export default {
   name: 'index',
   data () {
     return {
-      msg: '我是测试的文字'
+      msg: '今天把单位换算搞清楚了，明天开始写页面！'
     }
   }
 }
@@ -17,9 +18,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
+  // 单位换算；
+  //0.28rem = 14px; 0.24rem  = 12px;
+
   .box{
-    p {
+    .ceshi1 {
       color: red;
+      font-size: 14px;
+    }
+    .ceshi2{
+      color: green;
+      font-size: 0.28rem;
     }
   }
 </style>
