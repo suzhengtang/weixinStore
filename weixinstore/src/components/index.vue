174470fb3c3ab5e36dca034a652791cfd0d752f4
suzhengtang <1037617576@qq.com>
2018-08-29 00:32:51 +0800

    标签栏OK

--- weixinstore/src/components/index.vue
+++ weixinstore/src/components/index.vue
@@ -1,7 +1,32 @@
 <template>
-  <div class="box">
-    <p class="ceshi1">{{ msg }}</p>
-    <p class="ceshi2">{{ msg }}</p>
+  <div class="index">
+    <div class="head">
+      <h4 class="head-title">{{title}}</h4>
+    </div>
+    <div class="content">
+      <p>{{content}}</p>
+      <p>{{content}}</p>
+    </div>
+    <div class="footer">
+      <ul class="footer-tab">
+        <li class="tab-li">
+          <img class="tab-li-img" src="../assets/images/home.png" alt="首页">
+          <p class="tab-li-text">首页</p>
+        </li>
+        <li class="tab-li">
+          <img class="tab-li-img" src="../assets/images/tongxl.png" alt="通讯录">
+          <p class="tab-li-text">通讯录</p>
+        </li>
+        <li class="tab-li">
+          <img class="tab-li-img" src="../assets/images/fax.png" alt="发现">
+          <p class="tab-li-text">发现</p>
+        </li>
+        <li class="tab-li">
+          <img class="tab-li-img" src="../assets/images/me.png" alt="我的">
+          <p class="tab-li-text">我的</p>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -10,25 +35,66 @@ export default {
   name: 'index',
   data () {
     return {
-      msg: '今天把单位换算搞清楚了，明天开始写页面！'
+      title: '微信',
+      content: "我是内容",
+      footer: "我是底部栏",
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="less" scoped>
+
   // 单位换算；
   //0.28rem = 14px; 0.24rem  = 12px;
 
-  .box{
-    .ceshi1 {
-      color: red;
-      font-size: 14px;
+  .index{
+    width: 100%;
+    height: 100%;
+    background-color: #fff;
+    .head{
+      width: 100%;
+      height: 0.88rem;
+      background-color: #000;
+      .head-title{
+        text-align: center;
+        line-height: 0.88rem;
+        color: #fff;
+        font-size: 0.34rem;
+      }
     }
-    .ceshi2{
-      color: green;
-      font-size: 0.28rem;
+    .content{
+      width: 100%;
+      height: auto;
+      background-color: #ccc;
+    }
+    .footer{
+      width: 100%;
+      height: 0.89rem;
+      background-color: #fff;
+      position: fixed;
+      left: 0;
+      bottom: 0;
+      .footer-tab{
+        width: 100%;
+        height: 100%;
+        border-top: 1px solid #dddddd;
+        .tab-li{
+          width: 25%;
+          height: 100%;
+          float: left;
+          text-align: center;
+          padding-top: 0.07rem;
+          .tab-li-img{
+            height: 0.48rem;
+            width: auto;
+            vertical-align: top;
+          }
+          .tab-li-text{
+            font-size: 0.1rem;
+          }
+        }
+      }
     }
   }
 </style>
