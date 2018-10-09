f267ffd9b6c940110f4ed2a7f74639adf381893a
suzhengtang <1037617576@qq.com>
2018-10-09 22:13:55 +0800

    测试

---
+++ weixinstore/src/components/me.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="me">
+        {{me}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "me",
+    data() {
+        return {
+            me: "我就是我！"
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
+
+
