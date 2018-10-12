a10d29b657ccb022156562a81b0bb3b6cb3d7aa1
suzhengtang <1037617576@qq.com>
2018-10-13 00:05:19 +0800

    上班遇到的问题

--- weixinstore/src/components/index.vue
+++ weixinstore/src/components/index.vue
@@ -56,20 +56,20 @@
 
 <script>
 export default {
-  name: 'index',
-  data () {
+  name: "index",
+  data() {
     return {
-      title: '微信',
+      title: "微信",
       home: "首页",
       tongxl: "通讯录",
       fax: "发现",
       me: "我的",
       search: "搜索",
       contentLi: "内容部分明天开始做！",
-      searchText: '',
+      searchText: "",
       statusShow: false,
-      dataMsg: [],
-    }
+      dataMsg: []
+    };
   },
   created() {
     // this.$http.post('http://localhost:8088/post', formData).then((response) => {
@@ -79,223 +79,275 @@ export default {
     //       console.log("error");
     //     // error callback
     // });
-    this.$http.get('/api/appData').then((response) => {
+    this.$http.get("/api/appData").then(
+      response => {
         this.dataMsg = response.data.data.dataList;
-    }, (response) => {
+      },
+      response => {
         console.log("error");
-    });
+      }
+    );
+
+    this.sortArr();
   },
   methods: {
     tabChange() {
       // alert("我是单页面还是多页面开发呢！");
       //以下是我上班（9/29）遇到的问题，解决方法如下：
       let arr = [];
-      let arrA = [{id: '1'},{id: '2'}];
-      let arrB = [{name: 'zs', age: '20', like: 'che', id: '1'},{name: 'ls', age: '22', like: 'fang', id: '2'}];
-      for(let i=0; i<arrA.length; i++){
+      let arrA = [{ id: "1" }, { id: "2" }];
+      let arrB = [
+        { name: "zs", age: "20", like: "che", id: "1" },
+        { name: "ls", age: "22", like: "fang", id: "2" }
+      ];
+      for (let i = 0; i < arrA.length; i++) {
         debugger;
-        for(let item in arrA[i]){
-          if(item == "name"){
+        for (let item in arrA[i]) {
+          if (item == "name") {
             arr = arrA;
-          }else{
+          } else {
             arr = arrB;
           }
         }
       }
       console.log(arr);
     },
-    onBlur() {   //失去焦点
-      if(this.searchText == ""){
+    onBlur() {
+      //失去焦点
+      if (this.searchText == "") {
         this.statusShow = false;
-      }else{
+      } else {
         this.statusShow = true;
       }
     },
-    onFocus() {   //获得焦点
+    onFocus() {
+      //获得焦点
       this.statusShow = true;
+    },
+
+    // 以下是我上班（10/12）遇到的问题，解决方法如下：
+
+// 传入一个数组
+// 按照特定方式格式化
+sortArr() {
+    var arr=[
+        {Code:'x',Quantity:1,ItemType:'normal'},
+        {Code:'x',Quantity:2,ItemType:'normal'},
+        {Code:'y',Quantity:1,ItemType:'spec'},
+        {Code:'x',Quantity:5,ItemType:'normal'},
+        {Code:'y',Quantity:2,ItemType:'spec'},
+        {Code:'z',Quantity:1,ItemType:'normal'}
+    ];
+    var _arr = [],
+        _t = [],
+        _tmp;
+        var newArr = [];
+    // 按照特定的参数将数组排序将具有相同值得排在一起
+    arr = arr.sort(function(a, b) {
+        var s = a["Code"],
+            t = b["Code"];
+        return s < t ? -1 : 1;
+    });
+    if ( arr.length ){
+        _tmp = arr[0]["Code"];
     }
-  }
+    // 将相同类别的对象添加到统一个数组
+    for (var i in arr) {
+        if ( arr[i]["Code"] === _tmp ){
+            _t.push( arr[i] );
+        } else {
+            _tmp = arr[i]["Code"];
+            _arr.push( _t );
+            _t = [arr[i]];
+        }
+    }
+    // 将最后的内容推出新数组
+    _arr.push( _t );
+    console.log(_arr.length);
+    newArr = _arr[0].concat(_arr[1],_arr[2]);
+    console.log(newArr);
 }
+  }
+};
 </script>
 
 <style lang="less" scoped>
+// 单位换算；
+//0.28rem = 14px; 0.24rem  = 12px;
 
-  // 单位换算；
-  //0.28rem = 14px; 0.24rem  = 12px;
-
-  .index{
+.index {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  .head {
     width: 100%;
-    height: 100%;
-    background-color: #fff;
-    .head{
-      width: 100%;
-      height: 0.88rem;
-      background-color: #000;
-      position: fixed;
-      left: 0;
-      top: 0;
-      z-index: 100;
-      .head-title{
-        text-align: center;
-        line-height: 0.88rem;
-        color: #fff;
-        font-size: 0.34rem;
-      }
-      .icon-more{
-        width: 0.4rem;
-        height:0.4rem;
-        position: absolute;
-        top: 50%;
-        margin-top: -0.2rem;
-        right: 0.2rem;
-        img{
-          width: 100%;
-          height: 100%;
-        }
+    height: 0.88rem;
+    background-color: #000;
+    position: fixed;
+    left: 0;
+    top: 0;
+    z-index: 100;
+    .head-title {
+      text-align: center;
+      line-height: 0.88rem;
+      color: #fff;
+      font-size: 0.34rem;
+    }
+    .icon-more {
+      width: 0.4rem;
+      height: 0.4rem;
+      position: absolute;
+      top: 50%;
+      margin-top: -0.2rem;
+      right: 0.2rem;
+      img {
+        width: 100%;
+        height: 100%;
       }
     }
-    .content{
+  }
+  .content {
+    width: 100%;
+    height: auto;
+    background-color: #eee;
+    margin-top: 0.88rem;
+    .content-search {
       width: 100%;
-      height: auto;
-      background-color: #eee;
-      margin-top: 0.88rem;
-      .content-search{
-        width: 100%;
-        height: 1.10rem;
-        border-bottom: 1px solid #cccccc;
-        padding-top: 0.2rem;
-        box-sizing: border-box;
-        .search-main{
-          width: 7.15rem;
-          height: 0.7rem;
-          margin: 0 auto;
-          position: relative;
-          .search-text{
-            width: 100%;
-            height: 100%;
-            border-radius: 0.10rem;
-            background-color: #fff;
-            padding: 0 0.2rem;
-            box-sizing: border-box;
-          }
-          .search-placeholder{
-            font-size: 0.32rem;
-            color: #888;
-            position: absolute;
-            left: 3.2rem;;
-            top: 0.12rem;
-            &:before{
-              content: '';
-              width: 0.32rem;
-              height: 0.32rem;
-              background: url("../assets/images/search.png") 0 0;
-              background-size: 0.32rem auto;
-              display: inline-block;
-              position: relative;
-              left: -0.1rem;
-              top: 0.04rem;
-            }
-          }
-          .status{
-            display: none;
-          }
-          .search-icon{
+      height: 1.1rem;
+      border-bottom: 1px solid #cccccc;
+      padding-top: 0.2rem;
+      box-sizing: border-box;
+      .search-main {
+        width: 7.15rem;
+        height: 0.7rem;
+        margin: 0 auto;
+        position: relative;
+        .search-text {
+          width: 100%;
+          height: 100%;
+          border-radius: 0.1rem;
+          background-color: #fff;
+          padding: 0 0.2rem;
+          box-sizing: border-box;
+        }
+        .search-placeholder {
+          font-size: 0.32rem;
+          color: #888;
+          position: absolute;
+          left: 3.2rem;
+          top: 0.12rem;
+          &:before {
+            content: "";
             width: 0.32rem;
             height: 0.32rem;
-            background: url("../assets/images/spick.png") 0 0;
+            background: url("../assets/images/search.png") 0 0;
             background-size: 0.32rem auto;
-            display: block;
-            position: absolute;
-            top: 0.18rem;
-            right: 0.2rem;
+            display: inline-block;
+            position: relative;
+            left: -0.1rem;
+            top: 0.04rem;
           }
         }
-      }
-      .content-main{
-        width: 100%;
-        height: 100%;
-        background-color: #fff;
-        padding-left: 0.2rem;
-        box-sizing: border-box;
-        .main-all{
-          width: 100%;
-          height: 100%;
-          .main-all-li{
-            width: 100%;
-            height: 1.36rem;
-            border-bottom: 1px solid #ddd;
-            box-sizing: border-box;
-            .li-left{
-              float: left;
-              width: 1.20rem;
-              height: 100%;
-              position: relative;
-              .li-left-head{
-                width: 1.01rem;
-                height: 1.01rem;
-                position: absolute;
-                left: 0;
-                top: 0;
-                bottom: 0;
-                margin: auto;
-              }
-            }
-            .li-right{
-              float: left;
-              width: 5.90rem;
-              height: 100%;
-              padding-top: 0.20rem;
-              box-sizing: border-box;
-              .li-right-title{
-                width: 100%;
-                height: 0.50rem;
-                h5{
-                  float: left;
-                  font-size: 0.34rem;
-                  font-weight: 400;
-                  color: #000;
-                }
-                span{
-                  float: right;
-                  font-size: 0.2rem;
-                  color: #aaa;
-                }
-              }
-              .li-right-dis{
-                color: #888;
-              }
-            }
-          }
+        .status {
+          display: none;
+        }
+        .search-icon {
+          width: 0.32rem;
+          height: 0.32rem;
+          background: url("../assets/images/spick.png") 0 0;
+          background-size: 0.32rem auto;
+          display: block;
+          position: absolute;
+          top: 0.18rem;
+          right: 0.2rem;
         }
       }
     }
-    .footer{
+    .content-main {
       width: 100%;
-      height: 0.89rem;
+      height: 100%;
       background-color: #fff;
-      position: fixed;
-      left: 0;
-      bottom: 0;
-      .footer-tab{
+      padding-left: 0.2rem;
+      box-sizing: border-box;
+      .main-all {
         width: 100%;
         height: 100%;
-        border-top: 1px solid #dddddd;
-        .tab-li{
-          width: 25%;
-          height: 100%;
-          float: left;
-          text-align: center;
-          padding-top: 0.07rem;
-          .tab-li-img{
-            height: 0.48rem;
-            width: auto;
-            vertical-align: top;
+        .main-all-li {
+          width: 100%;
+          height: 1.36rem;
+          border-bottom: 1px solid #ddd;
+          box-sizing: border-box;
+          .li-left {
+            float: left;
+            width: 1.2rem;
+            height: 100%;
+            position: relative;
+            .li-left-head {
+              width: 1.01rem;
+              height: 1.01rem;
+              position: absolute;
+              left: 0;
+              top: 0;
+              bottom: 0;
+              margin: auto;
+            }
           }
-          .tab-li-text{
-            font-size: 0.1rem;
+          .li-right {
+            float: left;
+            width: 5.9rem;
+            height: 100%;
+            padding-top: 0.2rem;
+            box-sizing: border-box;
+            .li-right-title {
+              width: 100%;
+              height: 0.5rem;
+              h5 {
+                float: left;
+                font-size: 0.34rem;
+                font-weight: 400;
+                color: #000;
+              }
+              span {
+                float: right;
+                font-size: 0.2rem;
+                color: #aaa;
+              }
+            }
+            .li-right-dis {
+              color: #888;
+            }
           }
         }
       }
     }
   }
+  .footer {
+    width: 100%;
+    height: 0.89rem;
+    background-color: #fff;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    .footer-tab {
+      width: 100%;
+      height: 100%;
+      border-top: 1px solid #dddddd;
+      .tab-li {
+        width: 25%;
+        height: 100%;
+        float: left;
+        text-align: center;
+        padding-top: 0.07rem;
+        .tab-li-img {
+          height: 0.48rem;
+          width: auto;
+          vertical-align: top;
+        }
+        .tab-li-text {
+          font-size: 0.1rem;
+        }
+      }
+    }
+  }
+}
 </style>
